7c1a861fe2ab9271b466cfe312ed036ff998eb7e
myDemo <1083099465@qq.com>
2017-10-31 21:55:38 +0800

    提交项目代码

---
+++ MyGitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyGitDemo
+//
+//  Created by liquan on 2017/10/31.
+//  Copyright © 2017年 liquan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
